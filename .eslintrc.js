32c13be68e9b2e08c57a96d68423da88a70cb2c6
gitliucong <2505086227@qq.com>
2022-08-26 19:23:44 +0800

    feat: login

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,14 @@ module.exports = {
 	env: {
 		node: true
 	},
-	extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		// "@vue/typescript/recommended",
+		// "@vue/prettier",
+		// "@vue/prettier/@typescript-eslint",
+		'plugin:prettier/recommended'
+	],
 	parserOptions: {
 		parser: '@babel/eslint-parser'
 	},
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+console.log(process)
 module.exports = defineConfig({
-	transpileDependencies: true
+	transpileDependencies: true,
+	lintOnSave: false,
+	productionSourceMap: false,
+	publicPath: './', //根目录
+	outputDir: 'dist', //输出文件地址
+	assetsDir: 'static', //静态资源文件放置
+	devServer: {
+		port: 8089, //端口号
+		https: false,
+		host: '0.0.0.0',
+		open: true,
+		//配置代理
+		proxy: {
+			'/api': {
+				changeOrigin: true,
+				target: 'http://www.baidu.com',
+				pathRewrite: {
+					'/api': ''
+				}
+			}
+		}
+	}
 })
